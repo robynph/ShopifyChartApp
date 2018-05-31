87840ef633c103be7ecb72a6ed12bde3ec198d90
robyn <robyn@redh.io>
2018-05-31 09:01:25 -0400

    update

--- client/App.js
+++ client/App.js
@@ -3,8 +3,6 @@ import { Page } from '@shopify/polaris';
 import { EmbeddedApp } from '@shopify/polaris/embedded';
 import Chart from './components/Chart';
 import Table from './components/Table';
-
-
 import data from './data/tableData.json';
 
 import RedhioTable from './components/RedhioTable';
@@ -18,88 +16,65 @@ class App extends Component {
     super();
     this.state = {
       chartData:{},
-      //tableData:[]
+
     }
   }
 
   componentWillMount(){
     this.getChartData();
-    //this.getTableData();
+
   }
 
-  // getTableData(){
-  //      axios.get('https://api.myjson.com/bins/l5pw3')
-  //            .then(res => {
-  //              const {keywordData} = response.data;
-  //
-  //              const tableData = {
-  //                labels: keywordData.map(k => k.category),
-  //                datasets: [
-  //                  {
-  //                    label: 'spectra',
-  //                    fill: false,
-  //                    data: keywordData.map(d => d.noOfSpectra),
-  //                    backgroundColor:'#4A148C',
-  //                  }
-  //                ]
-  //              }
-  //
-  //              this.setState({ tableData });
-  //              console.log(this.state.tableData);
-  //            });
-  //             }
-
-
-getChartData(){
-    //Ajax xalls here
-    this.setState({
-      chartData:{
-          labels: ['Boston', 'Worchester', 'Spring', 'Lowell', 'Cambridger', 'New York'],
-          datasets:[
-            {
-              label:'Population',
-              fill: false,
-              data:[
-                1175893,
-                1820589,
-                1503060,
-                1060519,
-                1061042,
-                950072
-              ],
-              backgroundColor:[
-                '#6666CC',
-                '#009933',
-                '#0000FF',
-                '#CC0066',
-                '#330033',
-                '#4A148C'
+    getChartData(){
+        //Ajax xalls here
+        this.setState({
+          chartData:{
+              labels: ['Boston', 'Worchester', 'Spring', 'Lowell', 'Cambridger', 'New York'],
+              datasets:[
+                {
+                  label:'Population',
+                  fill: false,
+                  data:[
+                    1175893,
+                    1820589,
+                    1503060,
+                    1060519,
+                    1061042,
+                    950072
+                  ],
+                  backgroundColor:[
+                    '#6666CC',
+                    '#009933',
+                    '#0000FF',
+                    '#CC0066',
+                    '#330033',
+                    '#4A148C'
+                  ]
+                },
+                {
+                    label:'NewPopulation',
+                    fill: false,
+                    data:[
+                      175893,
+                      820589,
+                      503060,
+                      1060519,
+                      4061042,
+                      2950072
+                    ],
+                    backgroundColor:[
+                      '#6666CC',
+                      '#009933',
+                      '#0000FF',
+                      '#CC0066',
+                      '#330033',
+                      '#4A148C'
+                    ]
+                }
               ]
-            },
-            {
-                label:'NewPopulation',
-                fill: false,
-                data:[
-                  175893,
-                  820589,
-                  503060,
-                  1060519,
-                  4061042,
-                  2950072
-                ],
-                backgroundColor:[
-                  '#6666CC',
-                  '#009933',
-                  '#0000FF',
-                  '#CC0066',
-                  '#330033',
-                  '#4A148C'
-                ]
             }
-          ]
-        }
-    });
-  }
+        });
+      }
 
   render() {
     const { apiKey, shopOrigin } = window;
@@ -114,7 +89,7 @@ getChartData(){
           <ApiConsole />
           <RedhioTable />
           <Chart chartData={this.state.chartData} />
-
+          <Table data={data} />
         </Page>
       </EmbeddedApp>
     );
--- client/components/Table.js
+++ client/components/Table.js
@@ -34,7 +34,7 @@ export default function Table(props){
         }
       </tbody>
     </table>
-    
+
     </div>
   )
 }
